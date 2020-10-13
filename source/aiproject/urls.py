6a792032960e78a8499a91bf7135575f84da1a5f
hwooson <hwooson@onionfive.io>
2020-10-13 22:29:55 +0900

    redirect to index from accessing root path

--- source/aiproject/urls.py
+++ source/aiproject/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
 
 urlpatterns = [
+    path('', lambda _: redirect('index')),
     path('index', views.index, name='index')
 ]
